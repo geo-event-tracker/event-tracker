14bbc0e4a1666a6d37ef900eddaaf84ee0f2862a
ivan0313 <ivan0313@connect.hku.hk>
2022-02-15 00:23:14 +0800

    feat: generated components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { EventMapComponent } from './event-map/event-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    NavBarComponent,
+    EventMapComponent
   ],
   imports: [
     BrowserModule,
