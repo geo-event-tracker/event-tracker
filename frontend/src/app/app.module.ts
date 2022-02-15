9f61747f33640887fa24e03fad5bc363db300dc0
Ivan Ng <97230812+ivan-ngchakming@users.noreply.github.com>
2022-02-15 21:11:53 +0800

    feat: created component placeholders (#2)
    * feat: generated components
    
    * feat: minimal styling to setup componet layout
    
    * fix: removed nav bar placeholder
    
    * fix: fill spaces

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventMapComponent } from './event-map/event-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventMapComponent
   ],
   imports: [
     BrowserModule,
