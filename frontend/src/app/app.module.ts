ab094f9a91f6db79cb98c35480da640857a0e647
ivan0313 <ivan0313@connect.hku.hk>
2022-02-15 18:13:25 +0800

    fix: removed nav bar placeholder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { EventMapComponent } from './event-map/event-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
-    NavBarComponent,
     EventMapComponent
   ],
   imports: [
