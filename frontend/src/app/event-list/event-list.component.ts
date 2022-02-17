46ac9e68d1d6e3fbb74d0031a94a62c97cbdb763
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2022-02-17 08:05:26 +0100

    demo: delete me

--- frontend/src/app/event-list/event-list.component.ts
+++ frontend/src/app/event-list/event-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { GeoEventsService } from '../modules/data/services/geo-events.service';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+  styleUrls: ['./event-list.component.scss'],
 })
 export class EventListComponent implements OnInit {
+  geoEvents$ = this.geoEventsService.getAll();
 
-  constructor() { }
+  constructor(private readonly geoEventsService: GeoEventsService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
