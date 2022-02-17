497f76c4b4b7c1921d76528d9b40a8f47a1a49cc
ivan0313 <ivan0313@connect.hku.hk>
2022-02-17 21:22:54 +0800

    feat: display geoEvent data properly

--- frontend/src/app/event-list/event-list.component.ts
+++ frontend/src/app/event-list/event-list.component.ts
@@ -1,4 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+// TODO: replace by GeoEventService, blocked by #9
+const createMockGeoEvent = (props: any) => ({
+  id: '0',
+  center: {
+    latitude: 48.858093,
+    longitude: 2.294694,
+  },
+  title: 'Start of construction of the Eiffel Tower',
+  timestamp: new Date(2022, 1, 15, 20, 50),
+  ...props
+})
+
+// TODO: replace by GeoEventService, blocked by #9
+const today = new Date();
+const geoEventMocks = new BehaviorSubject([
+  createMockGeoEvent({ id: 0, address: '1 Street, City Name, Country' }),
+  createMockGeoEvent({ id: 1, timestamp: new Date(today.getTime() - 1000 * 24) }),
+  createMockGeoEvent({ id: 2, timestamp: new Date(today.getTime() - 1000 * 60 * 1) }),
+  createMockGeoEvent({ id: 3, timestamp: new Date(today.getTime() - 1000 * 60 * 50) }),
+  createMockGeoEvent({ id: 4, timestamp: new Date(today.getTime() - 1000 * 60 * 60) }),
+  createMockGeoEvent({ id: 5, timestamp: new Date(today.getTime() - 1000 * 60 * 60 * 23) }),
+  createMockGeoEvent({ id: 6, timestamp: new Date(today.getTime() - 1000 * 60 * 60 * 24 * 10) }),
+  createMockGeoEvent({ id: 7, title: 'some stupidly long title wow its so long and useless why would ppl fill in something this long and then we have to deal with them ahhhhhhhhhhhhh'}),
+]);
 
 @Component({
   selector: 'app-event-list',
@@ -12,67 +38,40 @@ export class EventListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  geoEvents$ = [
-    {
-      id: '0',
-      center: {
-        latitude: 48.858093,
-        longitude: 2.294694,
-      },
-      title: 'Start of construction of the Eiffel Tower',
-      timestamp: new Date(1887, 1, 1887),
-      timePassed: '5 days ago',
-    },
-    {
-      id: '1',
-      title: 'End of construction of the Eiffel Tower',
-      timestamp: new Date(1887, 3, 1889),
-      center: {
-        latitude: 48.858093,
-        longitude: 2.294694,
-      },
-      timePassed: '5 days ago',
-    },
-    {
-      id: '2',
-      title: 'End of construction of the Eiffel Tower',
-      timestamp: new Date(1887, 3, 1889),
-      center: {
-        latitude: 48.858093,
-        longitude: 2.294694,
-      },
-      timePassed: '5 days ago',
-    },
-    {
-      id: '3',
-      title: 'End of construction of the Eiffel Tower',
-      timestamp: new Date(1887, 3, 1889),
-      center: {
-        latitude: 48.858093,
-        longitude: 2.294694,
-      },
-      timePassed: '5 days ago',
-    },
-    {
-      id: '4',
-      title: 'End of construction of the Eiffel Tower',
-      timestamp: new Date(1887, 3, 1889),
-      center: {
-        latitude: 48.858093,
-        longitude: 2.294694,
-      },
-      timePassed: '5 days ago',
-    },
-    {
-      id: '5',
-      title: 'End of construction of the Eiffel Tower',
-      timestamp: new Date(1887, 3, 1889),
-      center: {
-        latitude: 48.858093,
-        longitude: 2.294694,
-      },
-      timePassed: '5 days ago',
-    },
-  ];
+  getTimePassed = (timestamp: Date) => {
+    let currentTime = new Date().getTime() / 1000;
+    let eventTime = timestamp.getTime() / 1000;
+
+    let diff = currentTime - eventTime;
+
+    if (diff < 60) return `${diff.toFixed(0)} second${diff >= 2 ? 's' : ''}`;
+
+    diff = diff / 60;
+
+    if (diff < 60 ) return `${diff.toFixed(0)} minute${diff >= 2 ? 's' : ''}`;
+
+    diff = diff / 60;
+
+    if (diff < 24 ) return `${diff.toFixed(0)} hour${diff >= 2 ? 's' : ''}`;
+
+    diff = diff / 24;
+
+    return `${diff.toFixed(0)} day${diff >= 2 ? 's' : ''}`;
+  }
+
+  getAddress = (geoEvent: any) => {
+    const { address, center } = geoEvent; console.log(geoEvent)
+    if (address) return address;
+    return `${center.longitude} ${center.latitude}`
+  }
+
+  /**
+   * Format a Date object into a "yyyy.mm.dd h:mm" string
+   */
+  formatDate = (d: Date) => {
+    return d.getDate() + "." + (d.getMonth()+1) + "." + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+  }
+
+  geoEvents$ = geoEventMocks.asObservable();
 
 }
