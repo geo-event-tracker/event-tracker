cfe7ce48bb085cc5940b71a846cadf84acec374b
ivan0313 <ivan0313@connect.hku.hk>
2022-02-18 11:31:18 +0800

    fix: add `@angular/material` using ng cli

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventMapComponent } from './event-map/event-map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { EventMapComponent } from './event-map/event-map.component';
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
