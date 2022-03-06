18f0cd8f9e4a80231adb9dcded266f3f765d0d8a
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2022-03-06 01:30:27 +0100

    feat: NestJS serves Angular `/dist`

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,20 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { join as pjoin } from 'path'
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GeoEventResolver } from './resolvers/geo-event.resolver'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      // TODO: configure for production
+      rootPath: pjoin(
+        __dirname,
+        '../../../../frontend/dist/event-tracker-frontend',
+      ),
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: pjoin(__dirname, '../../../src/model/schema.gql'),
@@ -18,7 +25,7 @@ import { GeoEventResolver } from './resolvers/geo-event.resolver'
       },
     }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService, GeoEventResolver],
 })
 export class AppModule {}
